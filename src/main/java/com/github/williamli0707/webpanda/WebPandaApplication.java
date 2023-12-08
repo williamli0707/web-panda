be41305abd29c5f63f84b45a035c75d7ee97bac3
William Li <williamhli0707@gmail.com>
2023-12-08 14:59:29 -0800

    username and password requested from user

--- src/main/java/com/github/williamli0707/webpanda/WebPandaApplication.java
+++ src/main/java/com/github/williamli0707/webpanda/WebPandaApplication.java
@@ -15,6 +15,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import java.io.IOException;
+import java.util.Scanner;
 import java.util.prefs.Preferences;
 
 /**
@@ -40,8 +41,21 @@ public class WebPandaApplication implements CommandLineRunner, AppShellConfigura
 	public static Preferences preferences = Preferences.userNodeForPackage(WebPandaApplication.class);
 
 	public static void main(String[] args) throws IOException {
-
 		SpringApplication.run(WebPandaApplication.class, args);
+	}
+
+	public void run(String... args) throws IOException {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter Runestone username: ");
+		String user = in.nextLine();
+		System.out.println("Enter Runestone password: ");
+		String password = in.nextLine();
+		RunestoneAPI.user = user;
+		RunestoneAPI.password = password;
+		in.close();
+
+		RunestoneAPI.reset();
+
 
 		//Reset cookie every hour to prevent session from expiring
 		new Thread(() -> {
@@ -55,16 +69,11 @@ public class WebPandaApplication implements CommandLineRunner, AppShellConfigura
 			}
 		}).start();
 		//TODO scans might be broken during resetting, fix this later
-	}
 
-	public void run(String... args) {
 		version = tmp;
 //		codescanRepo.deleteAll();
 		MongoManager.repository = codescanRepo;
 		RunestoneAPI.timeDiffSensitivity = preferences.getInt("timeDiffSensitivity", 50);
 		RunestoneAPI.largeEditSensitivity = preferences.getInt("largeEditSensitivity", 50);
-//		System.out.println("Time diff sensitivity: " + RunestoneAPI.timeDiffSensitivity);
-//		System.out.println("Large edit sensitivity: " + RunestoneAPI.largeEditSensitivity);
-//		codescanRepo.save(new CodescanRecord(new ArrayList<>(), new ArrayList<>(), new String[] {"1", "2"}, new HashMap<>()));
 	}
 }
